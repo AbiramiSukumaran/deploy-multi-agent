63cb1c1ccfaac18983dace43b19e9e7cac11d96a
Abirami Sukumaran <abidsukumaran@gmail.com>
2025-04-25 17:31:53 +0530

    Create test.py

---
+++ test.py
@@ -0,0 +1,32 @@
+import vertexai
+from vertexai.preview import reasoning_engines
+from vertexai import agent_engines
+import os
+import warnings
+from dotenv import load_dotenv
+load_dotenv() 
+
+
+GOOGLE_CLOUD_PROJECT = os.environ["GOOGLE_CLOUD_PROJECT"]
+GOOGLE_CLOUD_LOCATION = os.environ["GOOGLE_CLOUD_LOCATION"]
+GOOGLE_API_KEY = os.environ["GOOGLE_API_KEY"]
+GOOGLE_GENAI_USE_VERTEXAI=os.environ["GOOGLE_GENAI_USE_VERTEXAI"]
+AGENT_NAME = "adk_renovation_agent"
+MODEL_NAME = "gemini-2.5-pro-preview-03-25"  # Or your preferred Gemini model
+warnings.filterwarnings("ignore")
+
+
+PROJECT_ID = GOOGLE_CLOUD_PROJECT
+agent_engine_id = "<<YOUR_AGENT_ENGINE_ID>>"
+vertexai.init(project=PROJECT_ID, location="us-central1")
+agent = agent_engines.get(agent_engine_id)
+print("**********************")
+print(agent)
+print("**********************")
+
+
+for event in agent.stream_query(
+    user_id="test_user",
+    message="I want you to check order status.",
+):
+    print(event)
